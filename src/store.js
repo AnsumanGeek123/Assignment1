ba3166a199b28dd718239ffcaf85be510b7cf154
Deepak2607 <deepakkumrawat8@gmail.com>
2021-03-14 13:05:55 +0530

    last commit

--- src/store.js
+++ src/store.js
@@ -1,18 +1,38 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from "redux-thunk";
 import billReducers from "./reducers";
 
-const initialState = {};
 
+const initialState = {};
 const middleware = [thunk];
 
+
+// const store = createStore(
+//   billReducers,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
+// export default store;
+
+
+
+//creating store
 const store = createStore(
-  billReducers,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+        billReducers,
+        initialState,
+        composeWithDevTools(applyMiddleware(...middleware))
 );
 
+
+store.subscribe(()=> {
+    console.log(store.getState());
+})
+
+
 export default store;
+
